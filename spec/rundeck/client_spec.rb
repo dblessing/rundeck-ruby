a776f109d09177fabd4c7bf973982b7ece765446
Drew Blessing <drew.blessing@me.com>
2014-09-01 15:18:33 -0500

    More tests for client

--- spec/rundeck/client_spec.rb
+++ spec/rundeck/client_spec.rb
@@ -4,18 +4,29 @@ describe Rundeck::Client do
   describe '#objectify' do
     subject { Rundeck::Client.new.objectify(result) }
 
-    context 'result is a hash' do
-      let(:result) { { a: 1, b: 2 } }
-      it { is_expected.to be_a Rundeck::ObjectifiedHash }
+    context 'when result' do
+      context 'is a hash' do
+        let(:result) { { a: 1, b: 2 } }
+        it { is_expected.to be_a Rundeck::ObjectifiedHash }
+      end
+
+      context 'is an array' do
+        let(:result) { [{ a: 1, b: 2 }, { c: 3, d: 4 }] }
+
+        it { is_expected.to be_a Array }
+        its([0]) { is_expected.to be_a Rundeck::ObjectifiedHash }
+        its([1]) { is_expected.to be_a Rundeck::ObjectifiedHash }
+      end
+
+      context 'is a string' do
+        let(:result) { 'A result' }
+        it do
+          expect do
+            Rundeck::Client.new.objectify('string')
+          end.to raise_error Rundeck::Error::Parsing,
+                             "Couldn't parse a response body"
+        end
+      end
     end
-
-    context 'result is an array' do
-      let(:result) { [{ a: 1, b: 2 }, { c: 3, d: 4 }] }
-
-      it { is_expected.to be_a Array }
-      its([0]) { is_expected.to be_a Rundeck::ObjectifiedHash }
-      its([1]) { is_expected.to be_a Rundeck::ObjectifiedHash }
-    end
-
   end
 end
