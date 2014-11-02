e0479356e8403ffbdaa4e0e1fa931e0e598ad2e1
Drew Blessing <drew.blessing@me.com>
2014-11-02 12:47:33 -0600

    Project support

--- lib/rundeck/client/project.rb
+++ lib/rundeck/client/project.rb
@@ -16,6 +16,66 @@ module Rundeck
       def projects(options = {})
         objectify get('/projects', options)['projects']
       end
+
+      # Create a project
+      #
+      # @see http://rundeck.org/docs/api/index.html#project-creation
+      #   Rundeck API documentation for 'POST /api/11/projects' endpoint
+      #
+      # @example
+      #   @TODO: Example
+      #
+      # @param  [String] content The job definition(s) as yaml or xml
+      # @param  [String] format The project creation format. 'json|xml',
+      #   defaults to 'json'
+      # @!macro options
+      # @return [Rundeck::ObjectifiedHash]
+      # @!macro exceptions
+      # def create_project(content, format = 'json', options = {})
+      #   unless format =~ /json|xml/
+      #     fail Error::InvalidAttributes, 'format must be yaml or xml'
+      #   end
+      #   options[:headers] = {} if options[:headers].nil?
+      #   options[:headers].merge!(
+      #       'Content-Type' => 'application/x-www-form-urlencoded')
+      #   options[:body] = "xmlBatch=#{content}"
+      #   options[:query] = {} if options[:query].nil?
+      #   options[:query]['format'] = format
+      #
+      #   objectify post('/jobs/import', options)['result']
+      # end
+
+      # Get a project by name
+      #
+      # @see http://rundeck.org/docs/api/index.html#getting-project-info
+      #   Rundeck API documentation for 'GET /api/1/project/[NAME]' endpoint
+      #
+      # @example
+      #   Rundeck.project('anvils')
+      #
+      # @param  [String] name The project name
+      # @!macro options
+      # @return [Rundeck::ObjectifiedHash]
+      # @!macro exceptions
+      def project(name, options = {})
+        objectify get("/project/#{name}", options)['project']
+      end
+
+      # Delete a project
+      #
+      # @see http://rundeck.org/docs/api/index.html#project-deletion
+      #   Rundeck API documentation for 'DELETE /api/11/project/[NAME]' endpoint
+      #
+      # @example
+      #   Rundeck.delete_project('my_project')
+      #
+      # @param  [String] name The project name
+      # @!macro options
+      # @return nil
+      # @!macro exceptions
+      def delete_project(name, options = {})
+        objectify delete("/project/#{name}", options)
+      end
     end
   end
 end
--- spec/rundeck/client/project_spec.rb
+++ spec/rundeck/client/project_spec.rb
@@ -7,10 +7,66 @@ describe Rundeck::Client do
     end
     subject { @projects }
 
-    its(:project) { is_expected.to be_an Array }
+    its(:project) { is_expected.to be_a Rundeck::ObjectifiedHash }
+
+    describe '#project' do
+      subject { @projects.project }
+
+      its(:name) { is_expected.to eq('anvils') }
+    end
 
     it 'expects a get to have been made' do
       expect(a_get('/projects')).to have_been_made
+    end
+  end
+
+  describe '.project' do
+    context 'when the project exists',
+            vcr: { cassette_name: 'project_valid' } do
+      before do
+        @project = Rundeck.project('anvils')
+      end
+      subject { @project }
+
+      it { is_expected.to be_a Rundeck::ObjectifiedHash }
+      its(:name) { is_expected.to eq('anvils') }
+    end
+
+    context 'when the project does not exist',
+            vcr: { cassette_name: 'project_invalid' } do
+      specify do
+        expect do
+          Rundeck.project('nonexistent')
+        end.to raise_error(Rundeck::Error::NotFound,
+                           /project does not exist/)
+      end
+    end
+  end
+
+  describe '.delete_project' do
+    context 'when a project exists',
+            vcr: { cassette_name: 'delete_project_valid' } do
+      before do
+        @project = Rundeck.delete_project(project)
+      end
+      let(:project) { 'anvils' }
+      subject { @project }
+
+      it { is_expected.to be_nil }
+
+      it 'expects a delete to have been made' do
+        expect(a_delete('/project/anvils')).to have_been_made
+      end
+    end
+
+    context 'when a project does not exist',
+            vcr: { cassette_name: 'delete_project_invalid' } do
+      specify do
+        expect do
+          Rundeck.delete_project('project1')
+        end.to raise_error(Rundeck::Error::NotFound,
+                           /Project does not exist/)
+      end
     end
   end
 end
